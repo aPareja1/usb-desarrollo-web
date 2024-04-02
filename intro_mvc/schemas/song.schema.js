d6bfb63e345b0d92391476ec53b3efb7ec2f0b66
Alejandro Pareja Londono <alejopar@gmail.com>
2024-04-02 09:20:01 -0500

    feat: schema validator

---
+++ intro_mvc/schemas/song.schema.js
@@ -0,0 +1,19 @@
+import Joi from "joi";
+
+export const songCreationSchema = Joi.object({
+    title: Joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+    artist: Joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+    album: Joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+    year: Joi.number().min(1600).max(new Date().getFullYear()),
+    genre: Joi.array().items(Joi.string()),
+    duration: Joi.string()
+})
