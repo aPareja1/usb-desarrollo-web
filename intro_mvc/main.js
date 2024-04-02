25ccc9eb68dd00a5807dde61171a1a56ec53b82b
Alejandro Pareja Londono <alejopar@gmail.com>
2024-04-02 09:57:40 -0500

    improve: add update & delete methods

--- intro_mvc/main.js
+++ intro_mvc/main.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { createRequire } from "node:module";
 import bodyParser from 'body-parser';
-import { songCreationSchema } from "./schemas/song.schema.js";
+import { songCreationSchema, songUpdateSchema } from "./schemas/song.schema.js";
+import { STATUS_CODES } from "node:http";
 
 const require = createRequire(import.meta.url);
 const readJSON = (path) => require(path);
@@ -54,6 +55,28 @@ app.post('/song', (req, res)=>{
     return res.json(songToAdd);
 })
 
+app.put('/song', (req, res)=>{
+    const body = req.body;
+    const data = songUpdateSchema.validate(body);
+    if(data.error){
+        return res.status(400).json(data.error.details[0].message);
+    }
+    const id= body.id;
+    const ref = songs.findIndex(x=> x.id === id );
+    songs[ref]= {
+        ...body,
+        id
+    }
+    return res.status(201).json(songs[ref]);
+})
+
+
+app.delete('/song/:id', (req, res)=>{
+    const {id} = req.params;
+    const index = songs.findIndex(x=> x.id === id );
+    songs.splice(index,1);
+    return res.status(200).json({message: 'Deleted'});
+})
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
 })
--- intro_mvc/schemas/song.schema.js
+++ intro_mvc/schemas/song.schema.js
@@ -17,3 +17,22 @@ export const songCreationSchema = Joi.object({
     genre: Joi.array().items(Joi.string()),
     duration: Joi.string()
 })
+
+export const songUpdateSchema = Joi.object({
+    id: Joi.string().required(),
+    title: Joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+    artist: Joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+    album: Joi.string()
+        .min(3)
+        .max(30)
+        .required(),
+    year: Joi.number().min(1600).max(new Date().getFullYear()),
+    genre: Joi.array().items(Joi.string()),
+    duration: Joi.string()
+})
